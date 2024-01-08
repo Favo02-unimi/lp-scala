9aab861e56f41f7432b7056c402ce8065e72a8ea
Luca Favini <luca.favini.02@gmail.com>
2024-01-08 17:00:25 +0100

    Exercises/Exams: update `csv`

--- Exercises/Exams/CSV/csv.scala
+++ Exercises/Exams/CSV/csv.scala
@@ -8,26 +8,16 @@ class CSVParser extends JavaTokenParsers {
   override val whiteSpace = " \t".r
 
   def start = rep(line) ~ lastLine ^^ {
-    case ((l1: List[List[String]]) ~ (l2: List[String])) => l1 :+ l2
+    case ((l1: List[List[String]]) ~ (l2: List[String])) => {
+      val list: List[List[String]] = l1 :+ l2
+      prettyPrint(list, getMaxWidthPerColumn(list))
+    }
   }
 
   def line = repsep(str, ",") <~ "\r?\n".r
   def lastLine = repsep(str, ",")
 
   def str = stringLiteral | "[^,\t\n\r]*".r
-}
-
-object Main {
-  def main(args : Array[String]): Unit = {
-    val p = new CSVParser()
-    args.foreach(file => {
-      p.parseAll(p.start, new FileReader(file)) match {
-        case p.Success(parsed: List[List[String]], _) =>
-          prettyPrint(parsed, getMaxWidthPerColumn(parsed))
-        case x => println(x)
-      }
-    })
-  }
 
   private def getMaxWidthPerColumn(table: List[List[String]]): Array[Int] = {
     def gmwpc(curIndex: Int, result: Array[Int]): Array[Int] = {
@@ -64,5 +54,17 @@ object Main {
     table.slice(1, table.length).foreach(l => printLine(l))
     printRep(lineLen, "-")
     print("\n")
+  }
+}
+
+object Main {
+  def main(args : Array[String]): Unit = {
+    val p = new CSVParser()
+    args.foreach(file => {
+      p.parseAll(p.start, new FileReader(file)) match {
+        case p.Success(parsed, _) =>
+        case x => println(x)
+      }
+    })
   }
 }
